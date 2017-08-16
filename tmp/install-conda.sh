b15cba60cd7324333e34b6d8642ea9fcb1b8f227
Moonchang Chae <mcchae@gmail.com>
2017-08-16 20:40:46 +0900

    remove tmp

--- tmp/install-conda.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#pyenv install miniconda3-3.10.1
-#pyenv install miniconda3-3.16.0
-#pyenv install miniconda3-3.19.0
-# https://repo.continuum.io/miniconda/Miniconda3-4.0.5-Linux-x86_64.sh
-pyenv install miniconda3-4.0.5
-# the above version has been failed!
